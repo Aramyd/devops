e42eb4c6c71fea9d00a1b8a01bca293b61868eef
Aramyd <aramyd@LAPTOP-I9MRGKMV.localdomain>
2024-05-06 22:04:23 -0500

    added script file tasks.sh

---
+++ tasks.sh
@@ -0,0 +1,18 @@
+file="production.txt"
+dir="production"
+touch $file
+echo "script task by tutor" > $file
+mkdir $dir
+ls
+rvs
+mv -v $file $dir
+ls $dir
+cd $dir
+cat $file
+cd ..
+rm -R $dir
+ls
+echo "task successfull"
+
+
+
